888d598fe254b6b4f58d5b246c74bc253df999cf
Arthur <arthur.dbs70@gmail.com>
2024-06-18 10:32:38 +0200

    test

---
+++ tests/index.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../index');
+
+describe('Test Routes', () => {
+    it('GET / should return Hello World', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello World!');
+    });
+
+    it('GET /hello should return Hello Jest', async () => {
+        const response = await request(app).get('/hello');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Hello Jest!');
+    });
+});
